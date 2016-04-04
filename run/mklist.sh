0f2c327e3531c1133a824a2ec4a45c0171306170
Ye Fang <yfang11@tigers.lsu.edu>
2016-04-03 22:52:17 -0500

    update documents

--- run/mklist.sh
+++ run/mklist.sh
@@ -1,7 +1,9 @@
 ./mkliglist.py ../data/astex/ligands/ > ../data/astex/ligands/list.txt
+grep "5cppA1.sdf" ../data/astex/ligands/list.txt > ../data/astex/ligands/lig_5cppA1.txt
 head -n 1 ../data/astex/ligands/list.txt > ../data/astex/ligands/ligs.txt
 head -n 1 ../data/astex/ligands/list.txt >> ../data/astex/ligands/ligs.txt
 head -n 1 ../data/astex/ligands/list.txt >> ../data/astex/ligands/ligs.txt
+
 
 
 ./mkprtlist.py ../data/astex/proteins/ > ../data/astex/proteins/list.txt
